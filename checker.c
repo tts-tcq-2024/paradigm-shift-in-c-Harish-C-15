ac73730e4a47e9e9e0c8724dbc275f52035f8e46
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-08-26 10:57:09 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -39,7 +39,7 @@ bool batteryIsOk(float temperature, float soc, float CR)
 }
 
 void main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+  assert(batteryIsOk(25, 70, 0.7) == true);
+  assert(batteryIsOk(50, 85, 0) == false);
   return null;
 }
