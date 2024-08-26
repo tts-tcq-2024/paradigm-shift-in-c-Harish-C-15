9f8b11e169c204e041d43ef7b96cedd625c83a99
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-08-26 11:15:05 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
-#include <iostream>
 
 bool temperature_checker(float temperature)
 {
   if(temperature < 0 || temperature > 45)
     {
-      cout<<"Temperature is out of range\n";
+      printf("Temperature is out of range\n");
       return false;
     }
   return true;
@@ -16,7 +15,7 @@ bool soc_checker(float soc)
 {
   if(soc < 20 || soc > 80)
     {
-      cout<<"State Of Charge is out of range\n";
+      printf("State Of Charge is out of range\n");
       return false;
     }
   return true;
@@ -26,7 +25,7 @@ bool cr_checker(float CR)
 {
   if(CR > 0.8)
     {
-      cout<<"Charge Rate is out of range\n";
+      printf("Charge Rate is out of range\n");
       return false;
     }
   return true;
