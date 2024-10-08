e74f16cf7ea129144bf75fe5bd8112e51821fab9
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-10-08 14:37:00 +0530

    Update Check_for_Limit.h

--- Check_for_Limit.h
+++ Check_for_Limit.h
@@ -1,1 +1,13 @@
+#ifndef CHECK_FOR_LIMIT_H
+#define CHECK_FOR_LIMIT_H
 
+#include <stdbool.h>
+
+namespace vfc
+{
+  bool   temperature_checker(float temperature);
+  bool soc_checker(float soc);
+  bool cr_checker(float CR);
+}
+
+#endif
