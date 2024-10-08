b0e8cc3bb15b63612ea2e5587cc10bc6673c1afc
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-10-08 14:28:28 +0530

    Update Check_for_Limit.c

--- Check_for_Limit.c
+++ Check_for_Limit.c
@@ -1,1 +1,31 @@
+namespace vfc
+{
+  bool temperature_checker(float temperature)
+  {
+    if(temperature < 0 || temperature > 45)
+    {
+      printf("Temperature is out of range\n");
+      return false;
+    }
+  return true;
+  }
 
+bool soc_checker(float soc)
+{
+  if(soc < 20 || soc > 80)
+    {
+      printf("State Of Charge is out of range\n");
+      return false;
+    }
+  return true;
+}
+
+bool cr_checker(float CR)
+{
+  if(CR > 0.8)
+    {
+      printf("Charge Rate is out of range\n");
+      return false;
+    }
+  return true;
+}
