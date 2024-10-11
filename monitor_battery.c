9a394b07af2de0036f8582017f53b8e1be7144ef
Harish-C-15 <harish1ga18cs060@gmail.com>
2024-10-11 09:21:08 +0530

    Update monitor_battery.c

--- monitor_battery.c
+++ monitor_battery.c
@@ -11,9 +11,7 @@ void printMessage(const char *message) {
 // Consolidated function to handle parameter checks
 void checkParameter(float value, float min, float max, ParameterState *state,
                     const char *errorMsg, const char *lowWarning, const char *highWarning,
-                    int checkHighWarningOnly) {
-    // Set Warning High as 0
-    int checkHighWarningOnly = 0
+                    int checkHighWarningOnly = 0;) {
     // Set the parameter state with error and warning messages
     setParameterState(state, errorMsg, lowWarning, highWarning);
 
